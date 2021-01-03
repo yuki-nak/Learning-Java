de14915178261d9f597ff67f13fb6f206e0be61f
yuki-nak <dpfcux@gmail.com>
2021-01-03 22:03:29 +0900

    Booo!!!

--- Test0001.java
+++ Test0001.java
@@ -1,9 +1,6 @@
 public class Test00001 {
     public static void main (String Args[]){
         //output text
-        System.out.println("Hello");
-        System.out.println("World");
-        System.out.println("Takoyaki");
-        System.out.println("Wasabi");
+        System.out.println("Wakasagi");
     }
 }
